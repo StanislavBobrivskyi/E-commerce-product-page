72fca9ef45b7ec49e5632db99e4ef80c4f925fc2
StanislavBobrivskyi <121600218+StanislavBobrivskyi@users.noreply.github.com>
2023-11-10 20:22:28 +0100

    add some information About section

--- src/Routs/About/About.jsx
+++ src/Routs/About/About.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-
+import { AboutSection } from 'components/About/AboutSection';
 export function About() {
   return (
     <div>
       <h2>Ласкаво просимо на сторінку з інформацією!</h2>
-      <About />
+      <AboutSection />
     </div>
   );
 }
--- src/components/About/AboutSection.jsx
+++ src/components/About/AboutSection.jsx
@@ -1,6 +1,7 @@
 import { OrderButton } from 'components/OrderBtn/OrderBtn.styled';
 import React from 'react';
 
-export function About() {
+export function AboutSection() {
+  <h1>here will be information about our products</h1>;
   return <OrderButton />;
 }
