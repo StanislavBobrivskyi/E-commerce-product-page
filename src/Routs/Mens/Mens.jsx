a5abc38ac09bd08373f2cf03f748fbef4675c8f3
StanislavBobrivskyi <121600218+StanislavBobrivskyi@users.noreply.github.com>
2023-11-11 16:44:36 +0100

    mens component add

--- src/Routs/Mens/Mens.jsx
+++ src/Routs/Mens/Mens.jsx
@@ -1,9 +1,11 @@
+import { MensSection } from 'components/Mans/MensSection';
 import React from 'react';
 
 export function Mens() {
   return (
     <div>
       <h2>Ласкаво просимо на сторінку з Чоловічим одягом !</h2>
+      <MensSection />
     </div>
   );
 }
---
+++ src/components/Mans/MensSection.jsx
@@ -0,0 +1,12 @@
+import { OrderButton } from 'components/OrderBtn/OrderBtn.styled';
+import React from 'react';
+
+export function MensSection() {
+  return (
+    <>
+      <OrderButton />
+      <p>1-st collection</p>
+      <p>2-nd collection</p>
+    </>
+  );
+}
