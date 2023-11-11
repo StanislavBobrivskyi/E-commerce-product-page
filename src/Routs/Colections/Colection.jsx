4d883368119546fbb79126f7305644b3d9c4d3da
StanislavBobrivskyi <121600218+StanislavBobrivskyi@users.noreply.github.com>
2023-11-11 16:38:58 +0100

    colection component add

--- src/Routs/Colections/Colection.jsx
+++ src/Routs/Colections/Colection.jsx
@@ -1,9 +1,11 @@
+import { ColectionSection } from 'components/Colections/ColectionsSection';
 import React from 'react';
 
 export function Colection() {
   return (
     <div>
       <h2>Ласкаво просимо на сторінку з Колекціями!</h2>
+      <ColectionSection />
     </div>
   );
 }
---
+++ src/components/Colections/ColectionsSection.jsx
@@ -0,0 +1,6 @@
+import { OrderButton } from 'components/OrderBtn/OrderBtn.styled';
+import React from 'react';
+
+export function ColectionSection() {
+  return <OrderButton />;
+}
