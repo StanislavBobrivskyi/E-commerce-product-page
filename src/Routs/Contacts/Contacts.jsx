1dfa9efb8e30e857273dba88ff06596f03c440a9
StanislavBobrivskyi <121600218+StanislavBobrivskyi@users.noreply.github.com>
2023-11-11 16:41:33 +0100

    contacts component add

--- src/Routs/Contacts/Contacts.jsx
+++ src/Routs/Contacts/Contacts.jsx
@@ -1,9 +1,11 @@
+import { ContactsSection } from 'components/ContactsSection/ContactsSection';
 import React from 'react';
 
 export function Contacts() {
   return (
     <div>
       <h2>Ласкаво просимо на сторінку з Контактами!</h2>
+      <ContactsSection />
     </div>
   );
 }
---
+++ src/components/ContactsSection/ContactsSection.jsx
@@ -0,0 +1,16 @@
+import { OrderButton } from 'components/OrderBtn/OrderBtn.styled';
+import React from 'react';
+
+export function ContactsSection() {
+  return (
+    <>
+      <OrderButton />
+      <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+        <li>4</li>
+      </ul>
+    </>
+  );
+}
