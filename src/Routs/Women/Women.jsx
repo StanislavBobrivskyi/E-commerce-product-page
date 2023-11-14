62f89eec37b8e2a6d1619fbc592036b9ae50da59
StanislavBobrivskyi <121600218+StanislavBobrivskyi@users.noreply.github.com>
2023-11-14 12:26:16 +0100

    add WomenSection

--- src/Routs/Women/Women.jsx
+++ src/Routs/Women/Women.jsx
@@ -1,9 +1,11 @@
+import { WomenSection } from 'components/WomenSection/WomenSection';
 import React from 'react';
 
 export function Women() {
   return (
     <div>
       <h2>Ласкаво просимо на сторінку з Жіночим одягом!</h2>
+      <WomenSection />
     </div>
   );
 }
