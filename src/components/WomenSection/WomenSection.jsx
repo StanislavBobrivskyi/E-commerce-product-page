c45574318679e10bdd02f83bf545bf8bbbd164ca
StanislavBobrivskyi <121600218+StanislavBobrivskyi@users.noreply.github.com>
2023-11-14 12:34:21 +0100

    add component data WomenSection

--- src/components/WomenSection/WomenSection.jsx
+++ src/components/WomenSection/WomenSection.jsx
@@ -1,13 +1,26 @@
 import React from 'react';
 
+import image1 from '../images/image-product-1-thumbnail.jpg';
+import image2 from '../images/image-product-2-thumbnail.jpg';
+import image3 from '../images/image-product-3-thumbnail.jpg';
+import image4 from '../images/image-product-4-thumbnail.jpg';
+
 export function WomenSection() {
   return (
     <div>
       <ul>
-        <li>1</li>
-        <li>2</li>
-        <li>3</li>
-        <li>4</li>
+        <li>
+          <img src={image1} alt="Description 1" />
+        </li>
+        <li>
+          <img src={image2} alt="Description 1" />
+        </li>
+        <li>
+          <img src={image3} alt="Description 1" />
+        </li>
+        <li>
+          <img src={image4} alt="Description 1" />
+        </li>
       </ul>
     </div>
   );
