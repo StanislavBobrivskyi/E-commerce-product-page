2bbee69ff9a4f50e59cbc58b32205a83dea5b263
StanislavBobrivskyi <121600218+StanislavBobrivskyi@users.noreply.github.com>
2023-11-26 14:23:22 +0100

    about us info

--- src/components/About/AboutSection.jsx
+++ src/components/About/AboutSection.jsx
@@ -3,5 +3,17 @@ import React from 'react';
 
 export function AboutSection() {
   <h1>here will be information about our products</h1>;
-  return <OrderButton />;
+  return (
+    <>
+      <OrderButton />
+      <ul>
+        <li>
+          <h3>Contacts information</h3>
+        </li>
+        <li>Email information</li>
+        <li>phone number</li>
+        <li>address line</li>
+      </ul>
+    </>
+  );
 }
