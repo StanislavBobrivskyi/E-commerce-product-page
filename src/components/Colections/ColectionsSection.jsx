5c0d9f50ef564c6ca088cacce48032ef76a0e321
StanislavBobrivskyi <121600218+StanislavBobrivskyi@users.noreply.github.com>
2023-11-26 14:26:07 +0100

    search line

--- src/components/Colections/ColectionsSection.jsx
+++ src/components/Colections/ColectionsSection.jsx
@@ -2,5 +2,14 @@ import { OrderButton } from 'components/OrderBtn/OrderBtn.styled';
 import React from 'react';
 
 export function ColectionSection() {
-  return <OrderButton />;
+  return (
+    <>
+      <OrderButton />
+      <h3>All colections</h3>
+      <form action="#">
+        <input type="text" placeholder="search colection" />
+        <button type="button">SEARCH</button>
+      </form>
+    </>
+  );
 }
